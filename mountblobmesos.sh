cd4486446b3e436647687d8ce265cc5218abaf0b
chai-NED <csadvisor@outlook.com>
2018-11-21 20:14:06 -0500

    Mount 3 blob fs for CSE

--- mountblobmesos.sh
+++ mountblobmesos.sh
@@ -10,6 +10,8 @@
 #
 
 
+#!/bin/sh
+
 #Debug 
 exec > /tmp/logcse 2>&1
 set -x
@@ -19,13 +21,21 @@ vmAdmin=$1
 storageAccountName=$2
 storageAccountKey=$3
 
-linuxMountPointLog="/mnt/azure/log" 
-tmpBlobFuseConfig="/home/$vmAdmin/blobfuse_tmp.cfg"
+logContainer="log"
+share1Container="share1"
+share2Container="share2"
+
+linuxMountPoint="/mnt/azure" 
+
 blobFuseConfigPath="/etc/blobfuse_azureblob.cfg"
+
+
 blobFuseConfigContentPrefix="accountName  $storageAccountName\naccountKey $storageAccountKey"
 blobFuseTempPath="/mnt/blobfusetmp"
 fsTabPath="/etc/fstab"
 mountScriptPath="/home/$vmAdmin/mount.sh"
+tmpBlobFuseConfig="/home/$vmAdmin/blobfuse_tmp.cfg"
+
 
 
 # # sudo wget https://packages.microsoft.com/config/ubuntu/18.04/packages-microsoft-prod.deb 
@@ -51,24 +61,28 @@ sudo mkdir -p $blobFuseTempPath
 sudo chown  $vmAdmin  $blobFuseTempPath
 
 
-# Build blobfuse config file
-blobFuseConfigContent="$blobFuseConfigContentPrefix\ncontainerName log"
-echo $blobFuseConfigContent > $tmpBlobFuseConfig
-sudo cp $tmpBlobFuseConfig $blobFuseConfigPath   
-sudo chmod 700  $blobFuseConfigPath
+# Build three blobfuse config files
 
-# Create a mountpoint and mount blob on it
-sudo mkdir -p  $linuxMountPointLog
-sudo /usr/bin/blobfuse  $linuxMountPointLog  --tmp-path=$blobFuseTempPath  --config-file=$blobFuseConfigPath  -o allow_other -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --log-level=LOG_DEBUG 
+for index in $logContainer $share1Container $share2Container
+do
+blobFuseConfigContent="$blobFuseConfigContentPrefix\ncontainerName $index"
+echo $blobFuseConfigContent > $tmpBlobFuseConfig.$index
+sudo cp $tmpBlobFuseConfig.$index $blobFuseConfigPath.$index   
+sudo chmod 700  $blobFuseConfigPath.$index
+
+# Create  mountpoint and mount blob on it
+sudo mkdir -p  "$linuxMountPoint/$index"
+sudo /usr/bin/blobfuse  $linuxMountPoint/$index  --tmp-path=$blobFuseTempPath  --config-file=$blobFuseConfigPath.$index  -o allow_other -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --log-level=LOG_DEBUG 
 
 #              
 # Persist blobfuse mountpount after a reboot. For that, you have to crate a mount.sh in home dir and use it in fstab
 #	
-# Generate mount.sh on the fly. 
-echo "/usr/bin/blobfuse $linuxMountPointLog  --tmp-path=$blobFuseTempPath  --config-file=$blobFuseConfigPath  -o allow_other -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --log-level=LOG_DEBUG" > $mountScriptPath
-chmod +x $mountScriptPath
+# Generate three mount.sh files on the fly. 
+echo "/usr/bin/blobfuse $linuxMountPoint/$index  --tmp-path=$blobFuseTempPath  --config-file=$blobFuseConfigPath.$index  -o allow_other -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --file-cache-timeout-in-seconds=120 --log-level=LOG_DEBUG" > $mountScriptPath.$index
+chmod +x $mountScriptPath.$index
       
-# Add a line to fstab  
+# Add each line  to fstab  
 sudo cp $fsTabPath $fsTabPath.orig
-sudo bash -c "echo $mountScriptPath  $linuxMountPointLog fuse _netdev >> $fsTabPath"
-	
+sudo bash -c "echo $mountScriptPath.$index  $linuxMountPoint/$index fuse _netdev >> $fsTabPath"
+
+done
